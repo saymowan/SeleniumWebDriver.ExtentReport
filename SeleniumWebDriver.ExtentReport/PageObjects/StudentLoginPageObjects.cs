9611d699f36e71eb40d9e940cf6c0af41267e1ad
Saymon Cristian Alves Oliveira <saymon.oliveira@animaeducacao.com.br>
2017-03-03 11:47:43 -0300

    Ajustes try catch

---
+++ SeleniumWebDriver.ExtentReport/PageObjects/StudentLoginPageObjects.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace SeleniumWebDriver.Basics
+{
+    //modificador de acesso public
+    public class StudentLoginPageObjects : WebDriver
+    {
+        //Tela mapeada: https://testmoz.com/1
+
+        //Método construtor para inicializar os elementos
+        public StudentLoginPageObjects()
+        {
+            PageFactory.InitElements(WebDriver.driver, this);
+        }
+
+        /// <summary>
+        /// Método para mapear um elemento
+        /// How: indica o tipo de elemento conforme encontrado na página web
+        /// Using: o valor do elemento na tela
+        /// IWebElement: elemento novo criado a partir de um mapeamento na tela
+        /// </summary>
+        [FindsBy(How = How.Id, Using = "id_student-name")]
+        public IWebElement inputYourName { get; set; }
+
+        //Mapeamento do botão da tela
+        [FindsBy(How = How.Name, Using = "student")]
+        public IWebElement btnContinue { get; set; }
+
+
+
+        /// <summary>
+        /// Método para inserir informação em campo
+        /// Tenta esoerar o elemento na tela
+        /// Limpa o campo
+        /// Envia informação
+        /// ERRO: não conseguiu fazer alguma das ações anteriores, o teste irá resultar em erro
+        /// </summary>
+        public void preencherName(string YourName)
+        {
+            try
+            {
+                SeleniumUteis.SeleniumUteis.esperarElemento(inputYourName);
+                inputYourName.Clear();
+                inputYourName.SendKeys(YourName);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }//fim void
+
+
+
+        public void clicarContinue()
+        {
+            try
+            {
+                SeleniumUteis.SeleniumUteis.esperarElemento(btnContinue);
+                btnContinue.Click();
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }//fim void
+
+
+    }
+}
---
+++ SeleniumWebDriver.ExtentReport/SeleniumUteis/SeleniumUteis.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace SeleniumWebDriver.Basics.SeleniumUteis
+{
+    public class SeleniumUteis
+    {
+
+        public static string UltimoErro = "";
+
+        public static String getPathSeleniumDriver()
+        {
+            // Função para definir o path do diretório
+            String strAppDir = Path.GetDirectoryName(
+            Assembly.GetExecutingAssembly().GetName().CodeBase).Substring(6);
+
+            //Função para reduzir em duas camadas a arvore do path
+            var gparent = Directory.GetParent(Directory.GetParent(strAppDir).ToString());
+
+            //Conversão var -> String
+            String aux = gparent.ToString();
+
+            //Concatenar Path diretorio + Path Drivers
+            String strAppFolderData = String.Concat(aux, "\\Drivers");
+
+            return strAppFolderData; //+ strAppFolderData;
+        }
+
+
+        // Método para esperar elemento "Espera explícita"
+        public static void esperarElemento(IWebElement elemento)
+        {
+            //Ajuste provisório no método SeleniumBase ->  WebDriver
+            WebDriverWait espera = new WebDriverWait(WebDriver.driver, TimeSpan.FromSeconds(100));
+            espera.Until(ExpectedConditions.ElementToBeClickable(elemento));
+            //Tenta validar se o elemento está clicavel
+            try
+
+            {
+                espera.Until(ExpectedConditions.ElementToBeClickable(elemento));
+            }
+            catch (Exception e)
+            {
+                UltimoErro = e.Message;
+                Assert.Fail("O elemento: '" + UltimoErro + "' não apareceu");
+            }
+        }//fim void
+
+
+    }
+}
